adb48e87f595a9c1fb7d712122970ba387a7af06
Alexander Borloev <borloev.1994@gmail.com>
2022-10-10 23:49:57 +0900

    Калькулятор отрисован в XAML и настроена работа кнопок 7,8,+,=

--- CalcWPF/MainWindow.xaml.cs
+++ CalcWPF/MainWindow.xaml.cs
@@ -20,9 +20,60 @@ namespace CalcWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        int num1 = 0;
+        int num2 = 0;
+        string op = "";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void bnt_7_Click(object sender, RoutedEventArgs e)
+        {
+            if(op == "")
+            {
+                num1 = num1 * 10 + 7;
+                txtValue.Text = num1.ToString();
+            }
+            else
+            {
+                num2 = num2 * 10 + 7;
+                txtValue.Text = num1.ToString();
+            }
+        }
+
+        private void bnt_8_Click(object sender, RoutedEventArgs e)
+        {
+            if (op == "")
+            {
+                num1 = num1 * 10 + 8;
+                txtValue.Text = num1.ToString();
+            }
+            else
+            {
+                num2 = num2 * 10 + 8;
+                txtValue.Text = num2.ToString();
+            }
+        }
+
+        private void bnt_plus_Click(object sender, RoutedEventArgs e)
+        {
+            op = "+";
+        }
+
+        private void bnt_eq_Click(object sender, RoutedEventArgs e)
+        {
+            int result = 0;
+            switch (op)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+            }
+
+            txtValue.Text = result.ToString();
+            op = "";
+            num1 = result;
         }
     }
 }
